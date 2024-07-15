6ba2091f5ac8591ed8897aa2a2afd5e3a4159592
Ana Anić Banić <git@ana.banic.hr>
2024-07-15 12:42:59 +0200

    Closes #1 and #2

--- frame_comparison_tool/__main__.py
+++ frame_comparison_tool/__main__.py
@@ -1,8 +1,30 @@
-from loguru import logger
+import cv2
+import random
+import argparse
+
+
+def display_random_frame(file: str) -> None:
+    vidcap = cv2.VideoCapture(file)
+
+    total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+    random_frame_idx = random.randint(0, total_frames)
+
+    vidcap.set(cv2.CAP_PROP_POS_FRAMES, random_frame_idx)
+
+    if vidcap.isOpened():
+        success, image = vidcap.read()
+
+        if success:
+            cv2.imshow(f'Random frame', image)
+            cv2.waitKey(0)
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    display_random_frame(args.file.name)
 
 
 if __name__ == '__main__':
