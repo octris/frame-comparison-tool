7bdc8b5dc0087ca13d8d1c6cfa007caafbae76a0
Ana Anić Banić <git@ana.banic.hr>
2024-07-19 12:47:01 +0200

    FrameLoader class update

--- frame_comparison_tool/frame_loader.py
+++ frame_comparison_tool/frame_loader.py
@@ -10,8 +10,7 @@ class FrameLoader:
     def __init__(self, file_path: Path):
         self._file_path: Path = file_path
         self._video_capture: cv2.VideoCapture = cv2.VideoCapture(str(self._file_path.absolute()))
-        self._curr_frame_idx: int = 0
-        self.frames: List[Image] = []
+        self._frames: List[Image] = []
 
     @property
     def file_name(self) -> str:
@@ -21,10 +20,9 @@ class FrameLoader:
     def total_frames(self) -> int:
         return int(self._video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    def _set_frame(self, frame_idx) -> None:
-        if self._video_capture.isOpened():
-            self._video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
-            self._curr_frame_idx = frame_idx
+    @property
+    def frames(self) -> List[Image]:
+        return self._frames
 
     def _get_frame(self) -> Optional[cv2.typing.MatLike]:
         if self._video_capture.isOpened():
@@ -40,22 +38,24 @@ class FrameLoader:
         frame_type = int(self._video_capture.get(cv2.CAP_PROP_FRAME_TYPE))
         return FrameType(frame_type)
 
-    def _get_composited_image(self) -> Image:
+    def _get_composited_image(self, frame_idx) -> Image:
+        if self._video_capture.isOpened():
+            self._video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+
         frame = self._get_frame()
         frame_type = self._get_frame_type()
         source = self.file_name
 
         frame = put_bordered_text(img=frame, text=f'SOURCE: {source}', origin=(0, 0))
         frame = put_bordered_text(img=frame,
-                                  text=f'FRAME TYPE: {frame_type}\nFRAME: {self._curr_frame_idx}/{self.total_frames}',
+                                  text=f'FRAME TYPE: {frame_type}\nFRAME: {frame_idx}/{self.total_frames}',
                                   origin=(frame.shape[1], 0), align=Align.RIGHT)
         return frame
 
     def sample_frames(self, frame_ids: List[int]) -> None:
-        if len(self.frames) > 0:
-            self.frames.clear()
+        if len(self._frames) > 0:
+            self._frames.clear()
 
         for idx in frame_ids:
-            self._set_frame(idx)
-            frame = self._get_composited_image()
-            self.frames.append(ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))))
+            frame = self._get_composited_image(idx)
+            self._frames.append(ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))))
