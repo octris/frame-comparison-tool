56e9b78919c899a9b00b36db13dd9495be667571
Ana Anić Banić <git@ana.banic.hr>
2024-11-22 15:00:45 +0100

    Crash CLI program when non-existing file is provided.

--- frame_comparison_tool/utils/argument_parser.py
+++ frame_comparison_tool/utils/argument_parser.py
@@ -1,16 +1,30 @@
 import argparse
-from argparse import Namespace
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+from typing import List
+
 from frame_comparison_tool.utils.frame_type import FrameType
 
 
-# TODO: Check choices in frame type
-
 def parse_arguments() -> Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--files', type=str, nargs='*', required=False)
+    parser: ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument('--files', type=Path, nargs='*', required=False)
     parser.add_argument('--n-samples', type=int, required=False, default=5)
     parser.add_argument('--seed', type=int, required=False, default=42)
     parser.add_argument('--frame-type', type=FrameType, choices=list(FrameType), required=False,
                         default="B-Type")
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    invalid_paths: List[str] = []
+
+    if args.files:
+        for file_path in args.files:
+            if not file_path.resolve().exists():
+                invalid_paths.append(str(file_path))
+
+    if invalid_paths:
+        invalid_paths_str = '\n'.join(path for path in invalid_paths)
+        parser.error(f'The following files do not exist:\n{invalid_paths_str}')
+
+    return args
